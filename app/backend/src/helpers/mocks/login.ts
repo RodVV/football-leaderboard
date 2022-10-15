3d7d58259092e08c652b9f0babde4014b4c96682
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-10-15 18:30:50 -0300

    add test rota login/validate

--- app/backend/src/helpers/mocks/login.ts
+++ app/backend/src/helpers/mocks/login.ts
@@ -1,6 +1,34 @@
-const loginMock = {
+import createToken from '../crypt/token';
+
+const userMock = {
+  id: 1,
+  username: 'Mock',
+  role: 'admin',
   email: 'mock@mock.com',
   password: 'mockword',
 };
 
-export default loginMock;
+const tokenMock = {
+  token: createToken(userMock.email),
+};
+
+const usersMockArray = {
+  users: [
+    {
+      id: 1,
+      username: 'Mock',
+      role: 'admin',
+      email: 'mock@mock.com',
+      password: 'mockword',
+    },
+    {
+      id: 2,
+      username: 'Mock2',
+      role: 'admin',
+      email: 'mock@mock2.com',
+      password: 'mockword2',
+    },
+  ],
+};
+
+export { userMock, tokenMock, usersMockArray };
--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -7,7 +7,8 @@ import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
-import loginMock from '../helpers/mocks/login';
+import { userMock, tokenMock, usersMockArray } from '../helpers/mocks/login';
+import User from '../database/models/User';
 
 chai.use(chaiHttp);
 
@@ -21,28 +22,45 @@ describe('/login', () => {
     });
 
     it('Deve efetuar login com sucesso', async () => {
-      const response = await chai.request(app).post('/login').send(loginMock);
+      const response = await chai.request(app).post('/login').send(userMock);
       expect(response.status).to.equal(200);
     });
     it('Deve retornar um token', async () => {
-      const response = await chai.request(app).post('/login').send(loginMock);
+      const response = await chai.request(app).post('/login').send(userMock);
       expect(response.status).to.equal(200);
       expect(response.body).to.have.property('token');
     });
     it('Deve retornar mensagem de erro se email nao for preenchido', async () => {
-      const response = await chai.request(app).post('/login').send({ ...loginMock, email: '' });
+      const response = await chai.request(app).post('/login').send({ ...userMock, email: '' });
       expect(response.status).to.equal(400);
       expect(response.body).to.deep.equal({ message: 'All fields must be filled' });
     });
     it('Deve retornar mensagem de erro se password nao for preenchido', async () => {
-      const response = await chai.request(app).post('/login').send({ ...loginMock, password: '' });
+      const response = await chai.request(app).post('/login').send({ ...userMock, password: '' });
       expect(response.status).to.equal(400);
       expect(response.body).to.deep.equal({ message: 'All fields must be filled' });
     });
     it('Deve retornar mensagem de erro se email nao for valido', async () => {
-      const response = await chai.request(app).post('/login').send({ ...loginMock, email: 'aaaaaaaaa' });
+      const response = await chai.request(app).post('/login').send({ ...userMock, email: 'aaaaaaaaa' });
       expect(response.status).to.equal(401);
       expect(response.body).to.deep.equal({ message: 'Incorrect email or password' });
+    });
+  });
+
+  describe('GET', () => {
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    before(() => {
+      sinon.stub(User, 'findOne').returns(userMock as any);
+    });
+
+    it('Deve retornar o user role', async () => {
+      const response = await chai.request(app).get('/login/validate').send(tokenMock);
+      expect(response.body.role).to.equal(userMock.role);
+      expect(response.status).to.equal(200);
     });
   });
  });
