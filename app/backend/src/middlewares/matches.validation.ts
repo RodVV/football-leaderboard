7c3c46b48c93879942b5569ba9594570544e03d9
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-10-17 03:36:24 -0300

    desbuga validation post/matches verifyTeams

--- app/backend/src/middlewares/matches.validation.ts
+++ app/backend/src/middlewares/matches.validation.ts
@@ -6,32 +6,34 @@ import User from '../database/models/User';
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
-const validMatch = {
+class ValidMatch {
+  private userModel = User;
+  private jwtModel = jwt;
 
-  async validToken(req: Request, res: Response, next: NextFunction) {
+  public async validToken(req: Request, res: Response, next: NextFunction) {
     const { authorization } = req.headers as IAuthorization;
     try {
-      const isValid = jwt.verify(authorization, JWT_SECRET);
+      const isValid = this.jwtModel.verify(authorization, JWT_SECRET);
       if (isValid) next();
     } catch (error) {
       return res.status(401).json({ message: 'Token must be a valid token' });
     }
-  },
+  }
 
-  async verifyTeams(req: Request, res: Response, next: NextFunction) {
+  public verifyTeams = async (req: Request, res: Response, next: NextFunction):
+  Promise<Response | undefined> => {
     const { homeTeam, awayTeam } = req.body;
-    const findHomeTeam = await User.findOne({ where: { homeTeam } });
-    const findAwayTeam = await User.findOne({ where: { awayTeam } });
-    if (!findHomeTeam || !findAwayTeam) {
-      return res.status(404).json({ message: 'There is no team with such id!' });
-    }
     if (homeTeam === awayTeam) {
       return res.status(401).json({
         message: 'It is not possible to create a match with two equal teams' });
     }
     next();
-  },
+  };
+}
 
-};
-
-export default validMatch;
+// const findHomeTeam = await this.userModel.findOne({ where: { homeTeam } });
+// const findAwayTeam = await this.userModel.findOne({ where: { awayTeam } });
+// if (!findHomeTeam || !findAwayTeam) {
+//   return res.status(404).json({ message: 'There is no team with such id!' });
+// }
+export default ValidMatch;
--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import MatchesService from '../services/MatchesService';
 import MatchesController from '../controllers/MatchesController';
-// import validMatch from '../middlewares/matches.validation';
+import ValidMatch from '../middlewares/matches.validation';
 // import User from '../database/models/User';
 
 const matchesRoute = Router();
 const matchesService = new MatchesService();
 const matchesController = new MatchesController(matchesService);
+const matchValidations = new ValidMatch();
 
 matchesRoute.get('/', matchesController.getAll);
-matchesRoute.post('/', matchesController.postMatch);
+matchesRoute.post('/', matchValidations.verifyTeams, matchesController.postMatch);
 matchesRoute.patch('/:id/finish', matchesController.finishMatch);
 
 export default matchesRoute;
